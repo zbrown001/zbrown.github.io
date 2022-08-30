f6ebcb96d7c640d2f31612f48da93616d8f76236
zbrown001 <zbrown@nanolos.com>
2022-08-30 16:32:50 -0600

    main

---
+++ myscripts.js
@@ -0,0 +1,26 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "300px";
+    document.getElementById("main").style.marginLeft = "300px";
+    }
+    
+    function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    }
+
+
+
+    var coll = document.getElementsByClassName("section");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
